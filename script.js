a4c670e7641e068b72e35826db5033c44d983cba
Alex Halbeisen <147077701+aldentaye@users.noreply.github.com>
2024-03-06 13:31:11 -0500

    define dog and background images

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const canvas = document.getElementById('game-canvas');
 // Get 2D rendering context
 const ctx = canvas.getContext('2d');
 
+const backgroundImage = new Image();
+backgroundImage.src = '/images/background.jpg';
+
+const dogImage = new Image();
+dogImage.src = '/images/dog.png';
+
 // Game variables
 let backgroundX = 0;
 let dogX = 50;
